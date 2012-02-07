0aff62c6c50b6f67795659feabfd0183f7cd22e0
Rouven Weßling <me@rouvenwessling.de>
2012-02-07 23:27:19 +0100

    Remove uneccessary jimports().

--- installation/index.php
+++ installation/index.php
@@ -61,7 +61,6 @@ require_once JPATH_LIBRARIES.'/import.php';
 require_once JPATH_LIBRARIES.'/cms.php';
 
 // Joomla library imports.
-jimport('joomla.database.table');
 jimport('joomla.environment.uri');
 jimport('joomla.utilities.arrayhelper');
 
