7ba61680f87dc282946a02c8664438aba3cc0678
Kyle Ledbetter <kyle@kyleledbetter.com>
2012-06-16 20:02:48 -0500

    - changing module menu assignment over to Chosen multi-select optgroups
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- administrator/components/com_modules/views/module/tmpl/edit_assignment.php
+++ administrator/components/com_modules/views/module/tmpl/edit_assignment.php
@@ -13,32 +13,7 @@ defined('_JEXEC') or die;
 require_once JPATH_ADMINISTRATOR.'/components/com_menus/helpers/menus.php';
 $menuTypes = MenusHelper::getMenuLinks();
 ?>
-		<script type="text/javascript">
-			window.addEvent('domready', function(){
-				validate();
-				document.getElements('select').addEvent('change', function(e){validate();});
-			});
-			function validate(){
-				var value	= document.id('jform_assignment').value;
-				var list	= document.id('menu-assignment');
-				if(value == '-' || value == '0'){
-					$$('.jform-assignments-button').each(function(el) {el.setProperty('disabled', true); });
-					list.getElements('input').each(function(el){
-						el.setProperty('disabled', true);
-						if (value == '-'){
-							el.setProperty('checked', false);
-						} else {
-							el.setProperty('checked', true);
-						}
-					});
-				} else {
-					$$('.jform-assignments-button').each(function(el) {el.setProperty('disabled', false); });
-					list.getElements('input').each(function(el){
-						el.setProperty('disabled', false);
-					});
-				}
-			}
-		</script>
+
 		<div class="control-group">
 			<label id="jform_menus-lbl" class="control-label" for="jform_menus"><?php echo JText::_('COM_MODULES_MODULE_ASSIGN'); ?></label>
 			<div id="jform_menus" class="controls">
@@ -50,53 +25,30 @@ $menuTypes = MenusHelper::getMenuLinks();
 		<div class="control-group">
 			<label id="jform_menuselect-lbl" class="control-label" for="jform_menuselect"><?php echo JText::_('JGLOBAL_MENU_SELECTION'); ?></label>
 			<div class="controls">
-				<div class="btn-toolbar">
-					<div class="btn-group">
-						<button class="btn" type="button" class="jform-assignments-button jform-rightbtn" onclick="$$('.chk-menulink').each(function(el) { el.checked = !el.checked; });">
-							<i class="icon-adjust"></i> <?php echo JText::_('JGLOBAL_SELECTION_INVERT'); ?>
-						</button>
-						<button class="btn" type="button" class="jform-assignments-button jform-rightbtn" onclick="$$('.chk-menulink').each(function(el) { el.checked = false; });">
-							<i class="icon-ban-circle"></i> <?php echo JText::_('JGLOBAL_SELECTION_NONE'); ?>
-						</button>
-						<button class="btn" type="button" class="jform-assignments-button jform-rightbtn" onclick="$$('.chk-menulink').each(function(el) { el.checked = true; });">
-							<i class="icon-plus-sign"></i> <?php echo JText::_('JGLOBAL_SELECTION_ALL'); ?>
-						</button>
-					</div>
-				</div>
-				
-				<ul id="menu-assignment" class="thumbnails">
+				<select id="menu-assignment" name="jform[assigned][]" multiple="multiple">
 					<?php foreach ($menuTypes as &$type) :
 						$count 	= count($type->links);
 						$i		= 0;
 						if ($count) :
 						?>
-						<li class="span3">
-							<div class="thumbnail">
-								<h5><?php echo $type->title;?></h5>
-								<?php
-								foreach ($type->links as $link) :
-									if (trim($this->item->assignment) == '-'):
-										$checked = '';
-									elseif ($this->item->assignment == 0):
-										$checked = ' checked="checked"';
-									elseif ($this->item->assignment < 0):
-										$checked = in_array(-$link->value, $this->item->assigned) ? ' checked="checked"' : '';
-									elseif ($this->item->assignment > 0) :
-										$checked = in_array($link->value, $this->item->assigned) ? ' checked="checked"' : '';
-									endif;
-								?>
-									<label class="checkbox small" for="link<?php echo (int) $link->value;?>">
-										<input type="checkbox" class="chk-menulink" name="jform[assigned][]" value="<?php echo (int) $link->value;?>" id="link<?php echo (int) $link->value;?>"<?php echo $checked;?>/> 
-										<?php echo $link->text; ?> <span class="label pull-right" rel="tooltip" title="Itemid"><?php echo (int) $link->value;?></span>
-									</label>
-								<?php if ($count > 20 && ++$i == ceil($count/2)) :?>
-									<hr />
-								<?php endif; ?>
-								<?php endforeach; ?>
-							</div>
-						</li>
-						<?php endif; ?>
-					<?php endforeach; ?>
-				</ul>
+				  <optgroup label="<?php echo $type->title;?>">
+					  	<?php
+					  	foreach ($type->links as $link) :
+					  		if (trim($this->item->assignment) == '-'):
+					  			$selected = '';
+					  		elseif ($this->item->assignment == 0):
+					  			$selected = ' selected="selected"';
+					  		elseif ($this->item->assignment < 0):
+					  			$selected = in_array(-$link->value, $this->item->assigned) ? ' selected="selected"' : '';
+					  		elseif ($this->item->assignment > 0) :
+					  			$selected = in_array($link->value, $this->item->assigned) ? ' selected="selected"' : '';
+					  		endif;
+					  	?>
+				    		<option value="<?php echo (int) $link->value;?>" <?php echo $selected;?>><?php echo $link->text; ?></option>
+				    <?php endforeach; ?>
+				  </optgroup>
+				 	 <?php endif; ?>
+				  <?php endforeach; ?>
+				</select>
 			</div>
 		</div>
