51643e030c649bb8ff87c3d5aad01e2e2812f038
Kyle Ledbetter <kledbetter@ebay.com>
2012-06-27 15:05:58 -0700

    - improving Site Configuration UX and Bootstrap markup, moving buttons up top, moving Sample data into collapse
    Signed-off-by: Kyle Ledbetter <kledbetter@ebay.com>

--- installation/views/site/tmpl/default.php
+++ installation/views/site/tmpl/default.php
@@ -8,149 +8,122 @@
 defined('_JEXEC') or die;
 ?>
 <div id="installer">
-	<fieldset>
-	<form action="index.php" method="post" id="adminForm" class="form-validate form-horizontal">
-			<legend><?php echo JText::_('INSTL_SITE'); ?></legend>
-			<div class="control-group">
-				<label for="" class="control-label">
-					<?php echo JText::_('INSTL_BASIC_SETTINGS'); ?>
-				</label>
-				<div class="controls">
-					<table class="table table-striped table-condensed">
-						<tbody>
-							<tr>
-								<td class="span2 item">
-									<?php echo $this->form->getLabel('site_name'); ?>
-								</td>
-								<td>
-									<?php echo $this->form->getInput('site_name'); ?>
-								</td>
-							</tr>
-							<tr>
-								<td class="item">
-									<?php echo $this->form->getLabel('admin_email'); ?>
-								</td>
-								<td>
-									<?php echo $this->form->getInput('admin_email'); ?>
-								</td>
-							</tr>
-							<tr>
-								<td class="item">
-									<?php echo $this->form->getLabel('admin_user'); ?>
-								</td>
-								<td>
-									<?php echo $this->form->getInput('admin_user'); ?>
-								</td>
-							</tr>
-							<tr>
-								<td class="item">
-									<?php echo $this->form->getLabel('admin_password'); ?>
-								</td>
-								<td>
-									<?php echo $this->form->getInput('admin_password'); ?>
-								</td>
-							</tr>
-							<tr>
-								<td class="item">
-									<?php echo $this->form->getLabel('admin_password2'); ?>
-								</td>
-								<td>
-									<?php echo $this->form->getInput('admin_password2'); ?>
-								</td>
-							</tr>
-						</tbody>
-						<tfoot>
-							<tr>
-								<td colspan="2"></td>
-							</tr>
-						</tfoot>
-					</table>
-				</div>
-			</div>
-			<div class="control-group">
-				<label for="" class="control-label">
-					<?php echo JText::_('INSTL_SITE_META_ADVANCED_SETTINGS'); ?>
-				</label>
-				<div class="controls">
-					<table class="table table-striped table-condensed">
-						<tbody>
-							<tr>
-								<td class="span2" title="<?php echo JText::_('INSTL_SITE_METADESC_TITLE_LABEL'); ?>">
-									<?php echo $this->form->getLabel('site_metadesc'); ?>
-								</td>
-								<td>
-									<?php echo $this->form->getInput('site_metadesc'); ?>
-								</td>
-							</tr>
-							<tr>
-								<td title="<?php echo JText::_('INSTL_SITE_METAKEYS_TITLE_LABEL'); ?>">
-									<?php echo $this->form->getLabel('site_metakeys'); ?>
-								</td>
-								<td>
-									<?php echo $this->form->getInput('site_metakeys'); ?>
-								</td>
-							</tr>
-							<tr>
-								<td title="<?php echo JText::_('INSTL_SITE_OFFLINE_TITLE_LABEL'); ?>">
-									<?php echo $this->form->getLabel('site_offline'); ?>
-								</td>
-								<td>
-									<?php echo $this->form->getInput('site_offline'); ?>
-								</td>
-							</tr>
-						</tbody>
-						<tfoot>
-							<tr>
-								<td colspan="2"></td>
-							</tr>
-						</tfoot>
-					</table>
-				</div>
-			</div>
-		<input type="hidden" name="task" value="setup.saveconfig" />
-		<?php echo JHtml::_('form.token'); ?>
-		<?php echo $this->form->getInput('sample_installed'); ?>
-		</form>
+		<div id="collapseSample" class="collapse">
+			<form enctype="multipart/form-data" action="index.php" method="post" class="form-horizontal" id="filename">
+				<h3>
+					<?php echo JText::_('INSTL_SITE_SAMPLE_LABEL'); ?>
+				</h3>
+				<?php echo $this->form->getInput('sample_file'); ?> 
+				 <div class="message inlineError" id="theDefaultError" style="display: none">
+				 	<dl>
+				 		<dt class="error"><?php echo JText::_('JERROR'); ?></dt>
+				 		<dd id="theDefaultErrorMessage"></dd>
+				 	</dl>
+				 </div>
 
-		<form enctype="multipart/form-data" action="index.php" method="post" class="form-horizontal" id="filename">
-			<div class="control-group">
-				<label for="" class="control-label">
-					<?php echo $this->form->getLabel('sample_file'); ?>
-				</label>
-				<div class="controls">
-					<?php echo $this->form->getInput('sample_file'); ?> <span id="theDefault">
-						<button class="btn btn-info" name="instDefault" onclick="Install.sampleData(this, <?php echo $this->form->getField('sample_file')->id;?>);"><i class="icon-upload icon-white"></i> <?php echo JText::_('INSTL_SITE_INSTALL_SAMPLE_LABEL'); ?></button>
-					</span>
-					 <i class="icon-info-sign" title="<?php echo JText::_('INSTL_SITE_INSTALL_SAMPLE_DESC'); ?>"></i>
-					 <div class="message inlineError" id="theDefaultError" style="display: none">
-					 	<dl>
-					 		<dt class="error"><?php echo JText::_('JERROR'); ?></dt>
-					 		<dd id="theDefaultErrorMessage"></dd>
-					 	</dl>
-					 </div>
-				</div>
-			</div>
-			<div class="control-group">
-				<label for="" class="control-label">
-					
-				</label>
-				<div class="controls small">
-					<div class="alert alert-info">
-						<?php echo JText::_('INSTL_SITE_LOAD_SAMPLE_DESC1'); ?>
+				<p class="help-block">
+					<?php echo JText::_('INSTL_SITE_LOAD_SAMPLE_DESC1'); ?>
+				</p>
+				<span id="theDefault">
+					<button class="btn btn-success" name="instDefault" onclick="Install.sampleData(this, <?php echo $this->form->getField('sample_file')->id;?>);"><i class="icon-upload icon-white"></i> <?php echo JText::_('INSTL_SITE_INSTALL_SAMPLE_LABEL'); ?></button>
+				</span>
+				<?php echo $this->form->getInput('type'); ?>
+				<?php echo JHtml::_('form.token'); ?>
+			</form>
+			<hr class="hr-condensed" />
+		</div>
+			<form action="index.php" method="post" id="adminForm" class="form-validate form-horizontal">
+				<div class="btn-toolbar">
+					<div class="btn-group">
+						<a class="btn" href="index.php?view=filesystem" onclick="return Install.goToPage('filesystem');" rel="prev" title="<?php echo JText::_('JPrevious'); ?>"><i class="icon-arrow-left"></i> <?php echo JText::_('JPrevious'); ?></a>
+						
+					</div>
+					<div class="btn-group">
+						<a class="btn btn-warning" href="#collapseSample" data-toggle="collapse"><i class="icon-upload icon-white"></i> <?php echo JText::_('INSTL_SITE_INSTALL_SAMPLE_LABEL'); ?></a>
+					</div>
+					<div class="btn-group">
+						<a  class="btn btn-primary"  href="#" onclick="Install.submitform();" rel="next" title="<?php echo JText::_('JNext'); ?>"><i class="icon-arrow-right icon-white"></i> <?php echo JText::_('JNext'); ?></a>
 					</div>
 				</div>
-			</div>
-			<div class="form-actions">
-			<?php if ($this->document->direction == 'ltr') : ?>
-					<span class="prev"><a class="btn" href="index.php?view=filesystem" onclick="return Install.goToPage('filesystem');" rel="prev" title="<?php echo JText::_('JPrevious'); ?>"><?php echo JText::_('JPrevious'); ?></a></span> 
-					<span class="next"><a class="btn btn-primary" href="#" onclick="Install.submitform();" rel="next" title="<?php echo JText::_('JNext'); ?>"><?php echo JText::_('JNext'); ?></a></span>
-			<?php elseif ($this->document->direction == 'rtl') : ?>
-					<span class="prev"><a class="btn btn-primary" href="#" onclick="Install.submitform();" rel="next" title="<?php echo JText::_('JNext'); ?>"><?php echo JText::_('JNext'); ?></a></span> 
-					<span class="next"><a class="btn" href="index.php?view=filesystem" onclick="return Install.goToPage('filesystem');" rel="prev" title="<?php echo JText::_('JPrevious'); ?>"><?php echo JText::_('JPrevious'); ?></a></span>
-			<?php endif; ?>
+				<h3><?php echo JText::_('INSTL_SITE'); ?></h3>
+				<hr class="hr-condensed" />
+				<div class="control-group">
+					<div class="control-label">
+						<?php echo $this->form->getLabel('site_name'); ?>
+					</div>
+					<div class="controls">
+						<?php echo $this->form->getInput('site_name'); ?>
+					</div>
 				</div>
-			<?php echo $this->form->getInput('type'); ?>
+				<div class="control-group">
+					<div class="control-label">
+						<?php echo $this->form->getLabel('admin_email'); ?>
+					</div>
+					<div class="controls">
+						<?php echo $this->form->getInput('admin_email'); ?>
+					</div>
+				</div>
+				<div class="control-group">
+					<div class="control-label">
+						<?php echo $this->form->getLabel('admin_user'); ?>
+					</div>
+					<div class="controls">
+						<?php echo $this->form->getInput('admin_user'); ?>
+					</div>
+				</div>
+				<div class="control-group">
+					<div class="control-label">
+						<?php echo $this->form->getLabel('admin_password'); ?>
+					</div>
+					<div class="controls">
+						<?php echo $this->form->getInput('admin_password'); ?>
+					</div>
+				</div>
+				<div class="control-group">
+					<div class="control-label">
+						<?php echo $this->form->getLabel('admin_password2'); ?>
+					</div>
+					<div class="controls">
+						<?php echo $this->form->getInput('admin_password2'); ?>
+					</div>
+				</div>
+				<div class="control-group">
+					<div class="control-label">
+						<?php echo $this->form->getLabel('site_metadesc'); ?>
+					</div>
+					<div class="controls">
+						<?php echo $this->form->getInput('site_metadesc'); ?>
+						<p class="help-block">
+							<?php echo JText::_('INSTL_SITE_METADESC_TITLE_LABEL'); ?>
+						</p>
+					</div>
+				</div>
+				<div class="control-group">
+					<div class="control-label">
+						<?php echo $this->form->getLabel('site_metakeys'); ?>
+					</div>
+					<div class="controls">
+						<?php echo $this->form->getInput('site_metakeys'); ?>
+						<p class="help-block">
+							<?php echo JText::_('INSTL_SITE_METAKEYS_TITLE_LABEL'); ?>
+						</p>
+					</div>
+				</div>
+				<div class="control-group">
+					<div class="control-label">
+						<?php echo $this->form->getLabel('site_offline'); ?>
+					</div>
+					<div class="controls">
+						<?php echo $this->form->getInput('site_offline'); ?>
+						<p class="help-block">
+							<?php echo JText::_('INSTL_SITE_OFFLINE_TITLE_LABEL'); ?>
+						</p>
+					</div>
+				</div>
+
+
+			<input type="hidden" name="task" value="setup.saveconfig" />
 			<?php echo JHtml::_('form.token'); ?>
-		</form>
-	</fieldset>
+			<?php echo $this->form->getInput('sample_installed'); ?>
+			</form>
 </div>
