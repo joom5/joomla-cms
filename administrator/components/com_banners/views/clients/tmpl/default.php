960dba500f17371c2e4c17e7cbccfdcd84be5edb
kyleledbetter <kyle@kyleledbetter.com>
2012-08-01 10:51:43 -0500

    - adding hidden labels, sort/order select boxes, hidden-phone columns and inline contextual menus to Banner Clients

--- administrator/components/com_banners/views/clients/tmpl/default.php
+++ administrator/components/com_banners/views/clients/tmpl/default.php
@@ -12,14 +12,30 @@ defined('_JEXEC') or die;
 JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
 JHtml::_('behavior.tooltip');
 JHtml::_('behavior.multiselect');
+JHtml::_('dropdown.init');
 
 $user		= JFactory::getUser();
 $userId		= $user->get('id');
 $listOrder	= $this->escape($this->state->get('list.ordering'));
 $listDirn	= $this->escape($this->state->get('list.direction'));
 $params		= (isset($this->state->params)) ? $this->state->params : new JObject;
+$archived	= $this->state->get('filter.published') == 2 ? true : false;
+$trashed	= $this->state->get('filter.published') == -2 ? true : false;
+$sortFields = $this->getSortFields();
 ?>
-
+<script type="text/javascript">
+	Joomla.orderTable = function() {
+		table = document.getElementById("sortTable");
+		direction = document.getElementById("directionTable");
+		order = table.options[table.selectedIndex].value;
+		if (order != '<?php echo $listOrder; ?>') {
+			dirn = 'asc';
+		} else {
+			dirn = direction.options[direction.selectedIndex].value;
+		}
+		Joomla.tableOrdering(order, dirn, '');
+	}
+</script>
 <form action="<?php echo JRoute::_('index.php?option=com_banners&view=clients'); ?>" method="post" name="adminForm" id="adminForm">
 	<div class="row-fluid">
 		<!-- Begin Sidebar -->
@@ -38,7 +54,8 @@ $params		= (isset($this->state->params)) ? $this->state->params : new JObject;
 				<hr />
 				<div class="filter-select">
 					<h4 class="page-header"><?php echo JText::_('JSEARCH_FILTER_LABEL');?></h4>
-					<select name="filter_state" class="span12 small" onchange="this.form.submit()">
+					<label for="filter_state" class="element-invisible"><?php echo JText::_('JOPTION_SELECT_PUBLISHED');?></label>
+					<select name="filter_state" id="filter_state" class="span12 small" onchange="this.form.submit()">
 						<option value=""><?php echo JText::_('JOPTION_SELECT_PUBLISHED');?></option>
 						<?php echo JHtml::_('select.options', JHtml::_('jgrid.publishedOptions'), 'value', 'text', $this->state->get('filter.state'), true);?>
 					</select>
@@ -50,36 +67,56 @@ $params		= (isset($this->state->params)) ? $this->state->params : new JObject;
 		<div class="span10">
 			<div id="filter-bar" class="btn-toolbar">
 				<div class="filter-search btn-group pull-left">
+					<label for="filter_search" class="element-invisible"><?php echo JText::_('COM_BANNERS_SEARCH_IN_TITLE');?></label>
 					<input type="text" name="filter_search" id="filter_search" placeholder="<?php echo JText::_('COM_BANNERS_SEARCH_IN_TITLE'); ?>" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" title="<?php echo JText::_('COM_BANNERS_SEARCH_IN_TITLE'); ?>" />
 				</div>
 				<div class="btn-group pull-left">
 					<button class="btn" rel="tooltip" type="submit" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>"><i class="icon-search"></i></button>
 					<button class="btn" rel="tooltip" type="button" title="<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>" onclick="document.id('filter_search').value='';this.form.submit();"><i class="icon-remove"></i></button>
 				</div>
+				<div class="btn-group pull-right hidden-phone">
+					<label for="limit" class="element-invisible"><?php echo JText::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC');?></label>
+					<?php echo $this->pagination->getLimitBox(); ?>
+				</div>
+				<div class="btn-group pull-right hidden-phone">
+					<label for="directionTable" class="element-invisible"><?php echo JText::_('JFIELD_ORDERING_DESC');?></label>
+					<select name="directionTable" id="directionTable" class="input-small" onchange="Joomla.orderTable()">
+						<option value=""><?php echo JText::_('JFIELD_ORDERING_DESC');?></option>
+						<option value="asc" <?php if ($listDirn == 'asc') echo 'selected="selected"'; ?>><?php echo JText::_('JGLOBAL_ORDER_ASCENDING');?></option>
+						<option value="desc" <?php if ($listDirn == 'desc') echo 'selected="selected"'; ?>><?php echo JText::_('JGLOBAL_ORDER_DESCENDING');?></option>
+					</select>
+				</div>
+				<div class="btn-group pull-right">
+					<label for="sortTable" class="element-invisible"><?php echo JText::_('JGLOBAL_SORT_BY');?></label>
+					<select name="sortTable" id="sortTable" class="input-medium" onchange="Joomla.orderTable()">
+						<option value=""><?php echo JText::_('JGLOBAL_SORT_BY');?></option>
+						<?php echo JHtml::_('select.options', $sortFields, 'value', 'text', $listOrder);?>
+					</select>
+				</div>
 			</div>
 			<div class="clearfix"> </div>
 			<table class="table table-striped">
 				<thead>
 					<tr>
-						<th width="1%">
+						<th width="1%" class="hidden-phone">
 							<input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" />
+						</th>
+						<th width="5%" class="center">
+							<?php echo JText::_('JSTATUS'); ?>
 						</th>
 						<th>
 							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_CLIENT', 'a.name', $listDirn, $listOrder); ?>
 						</th>
-						<th width="30%">
+						<th width="30%" class="hidden-phone">
 							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_CONTACT', 'contact', $listDirn, $listOrder); ?>
 						</th>
-						<th width="5%">
+						<th width="5%" class="hidden-phone">
 							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_ACTIVE', 'nbanners', $listDirn, $listOrder); ?>
 						</th>
-						<th width="5%" class="nowrap">
-							<?php echo JText::_('COM_BANNERS_HEADING_METAKEYWORDS'); ?>
-						</th>
-						<th width="10%">
+						<th width="10%" class="hidden-phone">
 							<?php echo JText::_('COM_BANNERS_HEADING_PURCHASETYPE'); ?>
 						</th>
-						<th width="1%" class="nowrap">
+						<th width="1%" class="nowrap hidden-phone">
 							<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
 						</th>
 					</tr>
@@ -100,38 +137,72 @@ $params		= (isset($this->state->params)) ? $this->state->params : new JObject;
 					$canChange	= $user->authorise('core.edit.state',	'com_banners') && $canCheckin;
 					?>
 					<tr class="row<?php echo $i % 2; ?>">
-						<td class="center">
+						<td class="center hidden-phone">
 							<?php echo JHtml::_('grid.id', $i, $item->id); ?>
 						</td>
-						<td class="nowrap">
+						<td class="center">
 							<?php echo JHtml::_('jgrid.published', $item->state, $i, 'clients.', $canChange);?>
-							<?php if ($item->checked_out) : ?>
-								<?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'clients.', $canCheckin); ?>
-							<?php endif; ?>
-							<?php if ($canEdit) : ?>
-								<a href="<?php echo JRoute::_('index.php?option=com_banners&task=client.edit&id='.(int) $item->id); ?>">
-									<?php echo $this->escape($item->name); ?></a>
-							<?php else : ?>
-									<?php echo $this->escape($item->name); ?>
-							<?php endif; ?>
 						</td>
-						<td class="small">
+						<td class="nowrap has-context">
+							<div class="pull-left">
+								<?php if ($item->checked_out) : ?>
+									<?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'clients.', $canCheckin); ?>
+								<?php endif; ?>
+								<?php if ($canEdit) : ?>
+									<a href="<?php echo JRoute::_('index.php?option=com_banners&task=client.edit&id='.(int) $item->id); ?>">
+										<?php echo $this->escape($item->name); ?></a>
+								<?php else : ?>
+										<?php echo $this->escape($item->name); ?>
+								<?php endif; ?>
+							</div>
+							<div class="pull-left">
+								<?php
+									// Create dropdown items
+				 					JHtml::_('dropdown.edit', $item->id, 'client.');
+				 					JHtml::_('dropdown.divider');
+				 					if ($item->state) :
+				 						JHtml::_('dropdown.unpublish', 'cb' . $i, 'clients.');
+				 					else :
+				 						JHtml::_('dropdown.publish', 'cb' . $i, 'clients.');
+				 					endif;
+
+				 					JHtml::_('dropdown.divider');
+
+				 					if ($archived) :
+				 						JHtml::_('dropdown.unarchive', 'cb' . $i, 'clients.');
+				 					else :
+				 						JHtml::_('dropdown.archive', 'cb' . $i, 'clients.');
+				 					endif;
+
+				 					if ($item->checked_out) :
+				 						JHtml::_('dropdown.checkin', 'cb' . $i, 'clients.');
+				 					endif;
+
+				 					if ($trashed) :
+				 						JHtml::_('dropdown.untrash', 'cb' . $i, 'clients.');
+				 					else :
+				 						JHtml::_('dropdown.trash', 'cb' . $i, 'clients.');
+				 					endif;
+
+				 					// render dropdown list
+				 					echo JHtml::_('dropdown.render');
+				 					?>
+							</div>
+						</td>
+						<td class="small hidden-phone">
 							<?php echo $item->contact;?>
 						</td>
-						<td class="center">
+						<td class="center hidden-phone">
 							<?php echo $item->nbanners; ?>
 						</td>
-						<td class="small">
-							<?php echo $item->metakey; ?>
-						</td>
-						<td class="small">
+						<td class="small hidden-phone">
 							<?php if ($item->purchase_type<0):?>
 								<?php echo JText::sprintf('COM_BANNERS_DEFAULT', JText::_('COM_BANNERS_FIELD_VALUE_'.$params->get('purchase_type')));?>
 							<?php else:?>
 								<?php echo JText::_('COM_BANNERS_FIELD_VALUE_'.$item->purchase_type);?>
 							<?php endif;?>
 						</td>
-						<td class="center">
+						<td class="center hidden-phone">
 							<?php echo $item->id; ?>
 						</td>
 					</tr>
