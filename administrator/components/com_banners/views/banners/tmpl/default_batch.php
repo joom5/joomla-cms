53c2dff05f68ee7f9a94ba3475fbc1086d431311
kyleledbetter <kyle@kyleledbetter.com>
2012-08-01 09:56:05 -0500

    - changing "Clear" to "Cancel" for batch modal

--- administrator/components/com_banners/views/banners/tmpl/default_batch.php
+++ administrator/components/com_banners/views/banners/tmpl/default_batch.php
@@ -38,7 +38,7 @@ $published = $this->state->get('filter.published');
 	</div>
 	<div class="modal-footer">
 		<button class="btn" type="button" onclick="document.id('batch-category-id').value='';document.id('batch-client-id').value='';document.id('batch-language-id').value=''" data-dismiss="modal">
-			<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>
+			<?php echo JText::_('JCANCEL'); ?>
 		</button>
 		<button class="btn btn-primary" type="submit" onclick="Joomla.submitbutton('banner.batch');">
 			<?php echo JText::_('JGLOBAL_BATCH_PROCESS'); ?>
--- administrator/components/com_content/views/articles/tmpl/default_batch.php
+++ administrator/components/com_content/views/articles/tmpl/default_batch.php
@@ -38,7 +38,7 @@ $published = $this->state->get('filter.published');
 	</div>
 	<div class="modal-footer">
 		<button class="btn" type="button" onclick="document.id('batch-category-id').value='';document.id('batch-access').value='';document.id('batch-language-id').value=''" data-dismiss="modal">
-			<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>
+			<?php echo JText::_('JCANCEL'); ?>
 		</button>
 		<button class="btn btn-primary" type="submit" onclick="Joomla.submitbutton('article.batch');">
 			<?php echo JText::_('JGLOBAL_BATCH_PROCESS'); ?>
