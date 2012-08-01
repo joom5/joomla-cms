6595676d5714b5cb38a2c1d17bb56499d8223cae
kyleledbetter <kyle@kyleledbetter.com>
2012-08-01 11:14:57 -0500

    - adding hidden labels, sort/order select boxes, and hidden-phone columns to Banner Tracks

--- administrator/components/com_banners/views/tracks/tmpl/default.php
+++ administrator/components/com_banners/views/tracks/tmpl/default.php
@@ -18,7 +18,21 @@ $user		= JFactory::getUser();
 $userId		= $user->get('id');
 $listOrder	= $this->escape($this->state->get('list.ordering'));
 $listDirn	= $this->escape($this->state->get('list.direction'));
+$sortFields = $this->getSortFields();
 ?>
+<script type="text/javascript">
+	Joomla.orderTable = function() {
+		table = document.getElementById("sortTable");
+		direction = document.getElementById("directionTable");
+		order = table.options[table.selectedIndex].value;
+		if (order != '<?php echo $listOrder; ?>') {
+			dirn = 'asc';
+		} else {
+			dirn = direction.options[direction.selectedIndex].value;
+		}
+		Joomla.tableOrdering(order, dirn, '');
+	}
+</script>
 <form action="<?php echo JRoute::_('index.php?option=com_banners&view=tracks'); ?>" method="post" name="adminForm" id="adminForm">
 	<div class="row-fluid">
 		<!-- Begin Sidebar -->
@@ -37,18 +51,21 @@ $listDirn	= $this->escape($this->state->get('list.direction'));
 				<hr />
 				<div class="filter-select">
 					<h4 class="page-header"><?php echo JText::_('JSEARCH_FILTER_LABEL');?></h4>
-					<select name="filter_client_id" class="span12 small" onchange="this.form.submit()">
+					<label for="filter_client_id" class="element-invisible"><?php echo JText::_('COM_BANNERS_SEARCH_IN_TITLE');?></label>
+					<select name="filter_client_id" id="filter_client_id" class="span12 small" onchange="this.form.submit()">
 						<option value=""><?php echo JText::_('COM_BANNERS_SELECT_CLIENT');?></option>
 						<?php echo JHtml::_('select.options', BannersHelper::getClientOptions(), 'value', 'text', $this->state->get('filter.client_id'));?>
 					</select>
 					<hr class="hr-condensed" />
+					<label for="filter_category_id" class="element-invisible"><?php echo JText::_('COM_BANNERS_SEARCH_IN_TITLE');?></label>
 					<?php $category = $this->state->get('filter.category_id');?>
-					<select name="filter_category_id" class="span12 small" onchange="this.form.submit()">
+					<select name="filter_category_id" id="filter_category_id" class="span12 small" onchange="this.form.submit()">
 						<option value=""><?php echo JText::_('JOPTION_SELECT_CATEGORY');?></option>
 						<?php echo JHtml::_('select.options', JHtml::_('category.options', 'com_banners'), 'value', 'text', $category);?>
 					</select>
 					<hr class="hr-condensed" />
-			        <select name="filter_type" class="span12 small" onchange="this.form.submit()">
+					<label for="filter_type" class="element-invisible"><?php echo JText::_('COM_BANNERS_SEARCH_IN_TITLE');?></label>
+			        <select name="filter_type" id="filter_type" class="span12 small" onchange="this.form.submit()">
 						<?php echo JHtml::_('select.options', array(JHtml::_('select.option', '0', JText::_('COM_BANNERS_SELECT_TYPE')), JHtml::_('select.option', 1, JText::_('COM_BANNERS_IMPRESSION')), JHtml::_('select.option', 2, JText::_('COM_BANNERS_CLICK'))), 'value', 'text', $this->state->get('filter.type'));?>
 					</select>
 				</div>
@@ -66,25 +83,44 @@ $listDirn	= $this->escape($this->state->get('list.direction'));
 					<label class="filter-hide-lbl" for="filter_end"><?php echo JText::_('COM_BANNERS_END_LABEL'); ?></label>
 					<?php echo JHtml::_('calendar', $this->state->get('filter.end'), 'filter_end', 'filter_end', '%Y-%m-%d' , array('size'=>10, 'onchange'=>"this.form.fireEvent('submit');this.form.submit()"));?>
 				</div>
+				<div class="btn-group pull-right hidden-phone">
+					<label for="limit" class="element-invisible"><?php echo JText::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC');?></label>
+					<?php echo $this->pagination->getLimitBox(); ?>
+				</div>
+				<div class="btn-group pull-right hidden-phone">
+					<label for="directionTable" class="element-invisible"><?php echo JText::_('JFIELD_ORDERING_DESC');?></label>
+					<select name="directionTable" id="directionTable" class="input-small" onchange="Joomla.orderTable()">
+						<option value=""><?php echo JText::_('JFIELD_ORDERING_DESC');?></option>
+						<option value="asc" <?php if ($listDirn == 'asc') echo 'selected="selected"'; ?>><?php echo JText::_('JGLOBAL_ORDER_ASCENDING');?></option>
+						<option value="desc" <?php if ($listDirn == 'desc') echo 'selected="selected"'; ?>><?php echo JText::_('JGLOBAL_ORDER_DESCENDING');?></option>
+					</select>
+				</div>
+				<div class="btn-group pull-right">
+					<label for="sortTable" class="element-invisible"><?php echo JText::_('JGLOBAL_SORT_BY');?></label>
+					<select name="sortTable" id="sortTable" class="input-medium" onchange="Joomla.orderTable()">
+						<option value=""><?php echo JText::_('JGLOBAL_SORT_BY');?></option>
+						<?php echo JHtml::_('select.options', $sortFields, 'value', 'text', $listOrder);?>
+					</select>
+				</div>
 			</div>
 			<div class="clearfix"> </div>
 			<table class="table table-striped">
 				<thead>
 					<tr>
 						<th class="title">
-							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_NAME', 'b.name', $listDirn, $listOrder); ?>
+							<?php echo JText::_('COM_BANNERS_HEADING_NAME');?>
 						</th>
 						<th width="20%" class="nowrap">
-							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_CLIENT', 'cl.name', $listDirn, $listOrder); ?>
+							<?php echo JText::_('COM_BANNERS_HEADING_CLIENT');?>
 						</th>
-						<th width="10%" class="nowrap">
-							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_TYPE', 'track_type', $listDirn, $listOrder); ?>
+						<th width="10%" class="nowrap hidden-phone">
+							<?php echo JText::_('COM_BANNERS_HEADING_TYPE');?>
 						</th>
-						<th width="10%" class="nowrap">
-							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_COUNT', 'count', $listDirn, $listOrder); ?>
+						<th width="10%" class="nowrap hidden-phone">
+							<?php echo JText::_('COM_BANNERS_HEADING_COUNT');?>
 						</th>
-						<th width="10%" class="nowrap">
-							<?php echo JHtml::_('grid.sort', 'JDATE', 'track_date', $listDirn, $listOrder); ?>
+						<th width="10%" class="nowrap hidden-phone">
+							<?php echo JText::_('JDATE');?>
 						</th>
 					</tr>
 				</thead>
@@ -107,13 +143,13 @@ $listDirn	= $this->escape($this->state->get('list.direction'));
 						<td>
 							<?php echo $item->client_name;?>
 						</td>
-						<td class="small">
+						<td class="small hidden-phone">
 							<?php echo $item->track_type==1 ? JText::_('COM_BANNERS_IMPRESSION'): JText::_('COM_BANNERS_CLICK');?>
 						</td>
-						<td>
+						<td class="hidden-phone">
 							<?php echo $item->count;?>
 						</td>
-						<td>
+						<td class="hidden-phone">
 							<?php echo JHtml::_('date', $item->track_date, JText::_('DATE_FORMAT_LC4').' H:i');?>
 						</td>
 					</tr>
