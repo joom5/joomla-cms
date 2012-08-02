5bc11a1e5b07cc086ce1a794b73a246c7c7ef54a
Anderson Grüdtner Martins <andergmartins@gmail.com>
2012-08-02 05:06:42 -0300

    Fix code style

--- templates/system/html/modules.php
+++ templates/system/html/modules.php
@@ -122,22 +122,26 @@ function modChrome_rounded($module, &$params, &$attribs)
  */
 function modChrome_outline($module, &$params, &$attribs)
 {
-	static $css=false;
+	static $css = false;
 	if (!$css)
 	{
-		$css=true;
+		$css = true;
+
 		jimport('joomla.environment.browser');
+
 		$doc = JFactory::getDocument();
 		$browser = JBrowser::getInstance();
 		$doc->addStyleDeclaration(".mod-preview-info { padding: 2px 4px 2px 4px; border: 1px solid black; position: absolute; background-color: white; color: red;}");
 		$doc->addStyleDeclaration(".mod-preview-wrapper { background-color:#eee; border: 1px dotted black; color:#700;}");
-		if ($browser->getBrowser()=='msie')
+		if ($browser->getBrowser() === 'msie')
 		{
-			if ($browser->getMajor() <= 7) {
+			if ($browser->getMajor() <= 7)
+			{
 				$doc->addStyleDeclaration(".mod-preview-info {filter: alpha(opacity=80);}");
 				$doc->addStyleDeclaration(".mod-preview-wrapper {filter: alpha(opacity=50);}");
 			}
-			else {
+			else
+			{
 				$doc->addStyleDeclaration(".mod-preview-info {-ms-filter: alpha(opacity=80);}");
 				$doc->addStyleDeclaration(".mod-preview-wrapper {-ms-filter: alpha(opacity=50);}");
 			}
@@ -150,11 +154,10 @@ function modChrome_outline($module, &$params, &$attribs)
 	}
 	?>
 	<div class="mod-preview">
-		<div class="mod-preview-info"><?php echo $module->position."[".$module->style."]"; ?></div>
+		<div class="mod-preview-info"><?php echo $module->position . "[" . $module->style . "]"; ?></div>
 		<div class="mod-preview-wrapper">
 			<?php echo $module->content; ?>
 		</div>
 	</div>
 	<?php
 }
-?>
